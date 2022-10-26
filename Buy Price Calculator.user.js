5ff6ef0069a636f5af1a2e7ba49cec4d790eac5f
xPokerr <67325900+xPokerr@users.noreply.github.com>
2022-10-26 15:19:04 +0200

    Update Buy Price Calculator.user.js

--- Buy Price Calculator.user.js
+++ Buy Price Calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Buy Price Calculator
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.1.0
 // @description  try to take over the world!
 // @author       Zano
 // @match        https://www.futbin.com/*
